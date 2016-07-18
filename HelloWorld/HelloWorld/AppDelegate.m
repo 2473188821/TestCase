f78d44e32107b13abbaadac1a899ab50071680fc
Chenfy <2473188821@qq.com>
2016-07-18 10:51:32 +0800

    git commit

--- HelloWorld/HelloWorld/AppDelegate.m
+++ HelloWorld/HelloWorld/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //reture Value
+    //git dev add
     return YES;
 }
 
