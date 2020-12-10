6de7738275e5b01d9f67af99f9110374e4a93c6c
Chenfy <chenfy@bokecc.com>
2020-12-10 11:34:20 +0800

    base func: 1、文件写入； 2、沙河路径获取 3、Present Controller 手势返回

--- A_TSS/TSS/Controllers/A_BaseController/HHBaseViewController.h
+++ A_TSS/TSS/Controllers/A_BaseController/HHBaseViewController.h
@@ -13,7 +13,34 @@ NS_ASSUME_NONNULL_BEGIN
 #define KSCREEN_WIDTH   ([UIScreen mainScreen].bounds.size.width)
 #define KSCREEN_HEIGTH  ([UIScreen mainScreen].bounds.size.height)
 
+//获取时间数据
+typedef NS_ENUM(NSInteger,CCTimeType) {
+    CCTimeTypeYear,
+    CCTimeTypeMonth,
+    CCTimeTypeDay,
+    CCTimeTypeHour,
+    CCTimeTypeMin,
+    CCTimeTypeSecond,
+    CCTimeTypeTimeStamp
+};
+
+//获取系统目录路径
+typedef NS_ENUM(NSInteger,CCDirectoryRootType) {
+    CCDirectoryRootTypeHome,
+    CCDirectoryRootTypeDocuments,
+    CCDirectoryRootTypeLibrary,
+    CCDirectoryRootTypeCache,
+    CCDirectoryRootTypeTemp,
+    CCDirectoryDayTypeCCInfo
+};
+
+
 @interface HHBaseViewController : UIViewController
+
+- (void)fun_addControllerBackGesture;
+- (void)fun_redirectNSLog;
+- (void)fun_addDataToFile;
+- (void)fun_getAppContainnerPath;
 
 @end
 
--- A_TSS/TSS/Controllers/A_BaseController/HHBaseViewController.m
+++ A_TSS/TSS/Controllers/A_BaseController/HHBaseViewController.m
@@ -18,20 +18,12 @@
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.lightGrayColor;
     
-    UIScreenEdgePanGestureRecognizer *edgeGes = [[UIScreenEdgePanGestureRecognizer alloc]  initWithTarget: self  action:@selector(edgePan:)];
-    edgeGes.edges = UIRectEdgeLeft;
-    [self.view addGestureRecognizer:edgeGes];
-    
+    [self fun_addControllerBackGesture];
     
     // Do any additional setup after loading the view.
 }
 
 
--(void)edgePan:(UIPanGestureRecognizer *)recognizer{
-    [self dismissViewControllerAnimated:YES completion:^{
-        
-    }];
-}
 /*
 #pragma mark - Navigation
 
@@ -41,5 +33,192 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark -- function 添加回退手势
+- (void)fun_addControllerBackGesture {
+    UIScreenEdgePanGestureRecognizer *edgeGes = [[UIScreenEdgePanGestureRecognizer alloc]  initWithTarget: self  action:@selector(edgePan:)];
+    edgeGes.edges = UIRectEdgeLeft;
+    [self.view addGestureRecognizer:edgeGes];
+}
+
+-(void)edgePan:(UIPanGestureRecognizer *)recognizer{
+    [self dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+ 
+#pragma mark -- function
+- (void)fun_getAppContainnerPath {
+    
+}
+
+#pragma mark -- 文件路径
+- (NSString *)rootDirectory:(CCDirectoryRootType)type
+{
+    switch (type) {
+        case CCDirectoryRootTypeHome:
+            return [self homeDirectory];
+            break;
+        case CCDirectoryRootTypeDocuments:
+            return [self documentDirectory];
+            break;
+        case CCDirectoryRootTypeLibrary:
+            return [self libraryDirectory];
+            break;
+        case CCDirectoryRootTypeTemp:
+            return [self tempDirectory];
+            break;
+        case CCDirectoryRootTypeCache:
+            return [self cacheDirectory];
+            break;
+        case CCDirectoryDayTypeCCInfo:
+            return [self directoryCCInfo];
+    }
+    return nil;
+}
+
+#pragma mark -- 根路径
+- (NSString *)homeDirectory {
+    // 获取沙盒根目录路径
+    NSString*homeDir = NSHomeDirectory();
+    return homeDir;
+}
+- (NSString *)documentDirectory {
+    // 获取Documents目录路径
+    NSString*docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES) firstObject];
+    return docDir;
+}
+- (NSString *)libraryDirectory {
+    //获取Library的目录路径
+    NSString*libDir = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory,NSUserDomainMask,YES) lastObject];
+    return libDir;
+}
+- (NSString *)cacheDirectory {
+    // 获取cache目录路径
+    NSString *cachesDir = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask,YES) firstObject];
+    return cachesDir;
+}
+- (NSString *)tempDirectory {
+    // 获取tmp目录路径
+    NSString*tmpDir =NSTemporaryDirectory();
+    return tmpDir;
+}
+
+// CCInfo
+- (NSString *)directoryCCInfo {
+    //获取Document文件
+    NSString * docsdir = [self documentDirectory];
+    //在Document目录下创建 "CCInfo" 文件夹
+    NSString *filePath = [docsdir stringByAppendingPathComponent:@"CCInfo"];
+    return filePath;
+}
+
+- (void)createDirectoryCCInfo {
+    //获取Document文件
+    NSString *docsdir = [self documentDirectory];
+    //在Document目录下创建 "CCInfo" 文件夹
+    NSString *dataFilePath = [docsdir stringByAppendingPathComponent:@"CCInfo"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    BOOL isDir = NO;
+    // fileExistsAtPath 判断一个文件或目录是否有效，isDirectory判断是否一个目录
+    BOOL existed = [fileManager fileExistsAtPath:dataFilePath isDirectory:&isDir];
+    if (!(isDir && existed))
+    {
+        // 在Document目录下创建一个archiver目录
+        [fileManager createDirectoryAtPath:dataFilePath withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+}
+
+//日期格式化
+- (NSString*)currentTimeString:(CCTimeType)type {
+    //[dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString *fm = @"";
+    switch (type) {
+        case CCTimeTypeYear: fm = @"yyyy";  break;
+        case CCTimeTypeMonth: fm = @"yyyyMM"; break;
+        case CCTimeTypeDay: fm = @"yyyyMMdd"; break;
+        case CCTimeTypeHour: fm = @"yyyy-MM-dd HH"; break;
+        case CCTimeTypeMin: fm = @"yyyy-MM-dd HH:mm"; break;
+        case CCTimeTypeSecond: fm = @"yyyy-MM-dd HH:mm:ss"; break;
+        case CCTimeTypeTimeStamp: fm = nil; break;
+    }
+    if (!fm) {
+        NSTimeInterval timestamp = [[NSDate date]timeIntervalSince1970];
+        NSString *str = [NSString stringWithFormat:@"%f",timestamp];
+        return str;
+    }
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    // ----------设置你想要的格式,hh与HH的区别:分别表示12小时制,24小时制
+    [formatter setDateFormat:fm];
+    //现在时间,你可以输出来看下是什么格式
+    NSDate *datenow = [NSDate date];
+    //----------将nsdate按formatter格式转成nsstring
+    NSString *currentTimeString = [formatter stringFromDate:datenow];
+//    XXLog(@"currentTimeString =  %@",currentTimeString);
+    
+    return currentTimeString;
+}
+
+#pragma mark -- 日志上报文件名
+- (NSString*)fileNameTimeStringMIN {
+    //[dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString *fm = @"yyyyMMdd_HHmm";
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    // ----------设置你想要的格式,hh与HH的区别:分别表示12小时制,24小时制
+    [formatter setDateFormat:fm];
+    //现在时间,你可以输出来看下是什么格式
+    NSDate *datenow = [NSDate date];
+    //----------将nsdate按formatter格式转成nsstring
+    NSString *currentTimeString = [formatter stringFromDate:datenow];
+//    XXLog(@"currentTimeString =  %@",currentTimeString);
+    return currentTimeString;
+}
+
+
+#pragma mark -- function 向文件写入数据
+- (void)fun_addDataToFile {
+    
+}
+- (void)fileAppendData:(NSData *)data path:(NSString *)path
+{
+    @try {
+        NSString *Path = path;
+        //追加数据
+        NSFileHandle *fileHandle=[NSFileHandle fileHandleForWritingAtPath:Path];
+        if (!fileHandle)
+        {
+            NSString *ss = @"-----start:----------";
+            NSData *data = [ss dataUsingEncoding:NSUTF8StringEncoding];
+            [data writeToFile:Path atomically:YES];
+            fileHandle=[NSFileHandle fileHandleForWritingAtPath:Path];
+        }
+        [fileHandle seekToEndOfFile];//跳到文件末尾
+        [fileHandle writeData:data];//写入
+        [fileHandle closeFile];//关闭
+    } @catch (NSException *exception) {
+        
+    } @finally {
+        
+    }
+}
+
+#pragma mark -- function NSLog文件重定向
+- (void)fun_redirectNSLog {
+    
+}
+
+- (void)redirectNSLogToDocumentFolder {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentDirectory = [paths objectAtIndex:0];
+    NSString *fileName =[NSString stringWithFormat:@"%@.log",[NSDate date]];
+    NSString *logFilePath = [documentDirectory stringByAppendingPathComponent:fileName];
+    NSLog(@"redirectNSLogToDocumentFolder-----:%@",logFilePath);
+    freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding],"a+",stderr);
+}
+
+
+
+
 
 @end
