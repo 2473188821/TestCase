52e8c36cece105042e7e95003cbc5d2ab0e50003
Chenfy <chenfy@bokecc.com>
2021-05-22 20:31:06 +0800

    add readme file

---
+++ modifyPositioning/modifyPositioning/README.h
@@ -0,0 +1,32 @@
+//
+//  README.h
+//  modifyPositioning
+//
+//  Created by Chenfy on 2021/5/22.
+//  Copyright © 2021 daiyazhou. All rights reserved.
+//
+
+#ifndef README_h
+#define README_h
+
+    一、工具
+    1、高德坐标拾取器： https://lbs.amap.com/console/show/picker
+    2、经纬度查询：    https://jingweidu.bmcx.com
+
+    二、地点
+    1、上海宜山路：121.3849957673268 , 31.167103637855593
+
+        <wpt lat="31.167103637855593" lon="121.3849957673268">
+
+    2、北京科大天工大厦： 116.34863848179205  , 39.98658037269078
+        
+        <wpt lat="39.98658037269078" lon="116.34863848179205">
+
+
+    3、北京--海淀文教产业园F座  116.35871072027967 , 39.96614926584091
+
+        <wpt lat="39.96614926584091" lon="116.35871072027967">
+
+
+
+#endif /* README_h */
