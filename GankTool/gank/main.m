4f028db225bdc79861a7a15ab73610e2941b6764
Chenfy <chenfy@bokecc.com>
2021-08-03 10:59:44 +0800

    test

---
+++ GankTool/gank/main.m
@@ -0,0 +1,36 @@
+//
+//  main.m
+//  gank
+//
+//  Created by Chenfy on 2021/7/17.
+//  Copyright © 2021 Chenfy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**x
+* 一、argc 参数个数
+* 二、argv 参数集合
+*  0、可执行文件路径
+*/
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+
+        NSLog(@" ");
+        NSLog(@"********** START *******************");
+        NSLog(@" ");
+
+        NSLog(@"Hello, World!");
+        
+        NSLog(@" ");
+        NSLog(@"********** END *******************");
+        NSLog(@" ");
+        
+        
+        
+        
+    }
+    return 0;
+}
